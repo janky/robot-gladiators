2465347eafcf1d2c37762b21ab62321b21436c9b
Kevin Thomas <kevin_thomas819@yahoo.com>
2021-12-22 22:49:16 -0500

    create and execute functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! Javascript is running!");
+var player_name = window.prompt("What is your robot's name?");
+
+console.log(player_name);
+
+function fight()
+{
+    window.alert("The fight has begun");
+}
+
+ fight();
